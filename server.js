a872948a8f146d8fdd3c763d05801d3eba7e1dd7
Mohamed Elssaffy <ma7amad123410000@gmail.com>
2021-09-12 17:35:08 +0200

    for prod

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -15,12 +16,19 @@ app.use(cors());
 app.options(cors());
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('App running'));
-
 app.use('/api/users', usersRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/posts', postsRoutes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
